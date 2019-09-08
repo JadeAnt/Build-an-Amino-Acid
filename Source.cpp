35d00342426328eefa056a90602d26ca19c9b035
Jaden Antoine <jadenjantoine@gmail.com>
2019-09-08 01:14:50 -0400

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -9,7 +9,11 @@ int main(){
   double nr_Oxygen, nr_Carbon, nr_Sulfur, nr_Hydrogen, nr_Nitrogen;
   double oxygen, carbon, sulfur, hydrogen, nitrogen;
   
-  
+  cout << "Input number of Oxygen atoms: "; cin << oxygen;
+  cout << "Input number of Carbon atoms: "; cin << carbon;
+  cout << "Input number of Sulfur atoms: "; cin << sulfur;
+  cout << "Input number of Hydrogen atoms: "; cin << hydrogen;
+  cout << "Input number of Nitrogen atoms: "; cin << nitrogen;
   
   
   double final_Amino_Acid = (nr_Oxygen * oxygen) + (nr_Nitrogen * nitrogen) + (nr_Carbon * carbon) + (nr_Sulfur * sulfur) + (nr_Hydrogen * hydrogen);
