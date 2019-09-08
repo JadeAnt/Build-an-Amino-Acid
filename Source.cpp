2dfa968f8f2051c425fd1edab7fde7b8cdb39016
Jaden Antoine <jadenjantoine@gmail.com>
2019-09-08 01:13:14 -0400

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -9,6 +9,9 @@ int main(){
   double nr_Oxygen, nr_Carbon, nr_Sulfur, nr_Hydrogen, nr_Nitrogen;
   double oxygen, carbon, sulfur, hydrogen, nitrogen;
   
+  
+  
+  
   double final_Amino_Acid = (nr_Oxygen * oxygen) + (nr_Nitrogen * nitrogen) + (nr_Carbon * carbon) + (nr_Sulfur * sulfur) + (nr_Hydrogen * hydrogen);
 
   cout << "The mass of this Amino Acid is: " << final_Amino_Acid << setprecision(3) << endl;
