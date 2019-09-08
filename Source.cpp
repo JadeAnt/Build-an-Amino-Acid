a8b5b5dd6b718da4f1b9886452923a9a363dd37f
Jaden Antoine <jadenjantoine@gmail.com>
2019-09-07 23:31:33 -0400

    Rename main.cpp to Source.cpp

---
+++ Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+  
+  double nr_Oxygen, nr_Carbon, nr_Sulfur, nr_Hydrogen, nr_Nitrogen;
+  double oxygen, carbon, sulfur, hydrogen, nitrogen;
+  
+  double final_Amino_Acid = (nr_Oxygen * oxygen) + (nr_Nitrogen * nitrogen) + (nr_Carbon * carbon) + (nr_Sulfur * sulfur) + (nr_Hydrogen * hydrogen);
+
+  cout << "The mass of this Amino Acid is: " << final_Amino_Acid << setprecision(3) << endl;
+
+  system("pause");
+  return 0;
+}
